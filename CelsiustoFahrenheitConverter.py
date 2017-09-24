2433f7f32ffe9865b748ac5be058f5a487bb5a54
Payton Dong Pei <silence1198@outlook.com>
2017-09-24 16:43:57 -0400

    Create CelsiustoFahrenheitConverter.py
    Converter

---
+++ CelsiustoFahrenheitConverter.py
@@ -0,0 +1,5 @@
+Temp = float(input("Please enter a number, the programme will convert it from celsus to Fahrenheit:\n"))
+A = Temp - 1.0
+B = A * 1.8 
+C = B + 33.8
+print("The Fahrenhit is %.1f" % C)
