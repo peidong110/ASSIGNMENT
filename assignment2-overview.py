f4d16c7ef8c486e69c1115babd692f3a16e01fa6
Your Name <you@example.com>
2017-09-25 17:21:50 -0400

    Add

---
+++ assignment2-overview.py
@@ -0,0 +1,25 @@
+# ============================================================
+#                                                             
+# Student Name (as it appears on cuLearn): Payton Pei     
+# Student ID (9 digits in angle brackets): <101065299>        
+# Course Code (for this current semester): COMP1405B          
+#                                                             
+# ============================================================c
+
+
+
+
+import sys
+
+
+# step 1
+temp = int(input("PLease enter a student#\n"))
+A = temp *2
+print (A)
+B = A + 9
+print(B)
+C = B - 3
+print(C)
+D = C / 2
+print(int(D))
+print(int(D-temp))
