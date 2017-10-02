7579a62dee20576b11d3ff2e361cf213a6ecbcc6
Payton Dong Pei <silence1198@outlook.com>
2017-10-02 00:16:11 -0400

    Update tutorial33.py

--- tutorial33.py
+++ tutorial33.py
@@ -1,6 +1,35 @@
 import pygame
-import sys
 import math
+from datetime import datetime
+
+
+print("Welcome to the Agency . .")
+name = input("What is your first name again?")
+surName = input("What is your Last name name again?")
+wage = float(input("Right, right.. Well, " + "Mr." + surName + " How much was your hourly wage?"))
+birthday = input("Hmm, I see.. and one last question: when was your birthday IN Year/Day/Month?")
+weeklyWage = wage * 0.6 * 40
+totalWeeks = int(365 / 7)
+yearlyWage = weeklyWage * totalWeeks
+
+today = datetime.today()
+birthdayVar = datetime.strptime(birthday, "%Y/%d/%m")
+
+myAge = today.year - birthdayVar.year
+
+myMonth = today.month - birthdayVar.month
+
+
+myday = today.day - birthdayVar.day
+
+print("Of course, how could I have forgotten!")
+print("Here's your ID card then:")
+print("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
+print("-- Name: ", surName, " ", name, "        −−-------")
+print("Income: ", yearlyWage, "$ / year", "-------------")
+print("Birthday in:                       ----")
+print(abs(myMonth), "Month,", abs(myMonth * 4), "Weeks,", abs(myday), "days-------------")
+print("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
 
 red = (255, 0, 0)
 black = (0, 0, 0)
@@ -13,36 +42,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     pygame.draw.circle(drawing_window, black, (350, 350), 350, 10)
+    endAngle = myday / (2 * math.pi)
+    pygame.draw.arc(drawing_window, red, (0, 0, 700, 700),abs(endAngle * 2 * math.pi), math.pi/2, 10)
     pygame.display.update()
-print("Welcome to the Agency . .")
-name = input("What is your first name again?")
-surName = input("What is your Last name name again?")
-wage = float(input("Right, right.. Well, " + "Mr." + surName + " How much was your hourly wage?"))
-birthday = input("Hmm, I see.. and one last question: when was your birthdayt IN Year/Day/Month?>")
-weeklyWage = wage * 0.6 * 40
-totalWeeks = int(365 / 7)
-yearlyWage = weeklyWage * totalWeeks
-from datetime import datetime
 
-today = datetime.today()
-birthdayVar = datetime.strptime(birthday, "%Y/%d/%m")
 
-myAge = today.year - birthdayVar.year
-print(myAge)
-
-myMonth = today.month - birthdayVar.month
-print(myMonth)
-
-myday = today.day - birthdayVar.day
-print(myday)
-'''print("pastday",str(birthdayVar.day))
-print("currentday"+str(today.day))'''
-
-print("Of course, how could I have forgotten!")
-print("Here's your ID card then:")
-print("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
-print("-- Name: ", surName, " ", name, "        −−-------")
-print("Income: ", yearlyWage, "$ / year", "-------------")
-print("Birthday in:                       ----")
-print(abs(myMonth), "Month,", abs(myMonth * 4), "Weeks,", abs(myday), "days-------------")
-print("−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−")
