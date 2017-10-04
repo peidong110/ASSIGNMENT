2969354169796283c99ac672bae07c19f0613d68
Payton Dong Pei <silence1198@outlook.com>
2017-10-04 14:04:53 -0400

    assignment4

---
+++ assignment.py
@@ -0,0 +1,18 @@
+'''
+Mason   hair tatto eyepatch glasses earing pipe
+Alex	hat hair tatto eyepatch glasses moustache pipe earring
+Reagan  hair eyepatch tatto glasses moustache pipe
+TYler	
+Daryl   hat hair tatto eyepatch glasses earing pipe
+Amari	hat, hair, eyepatch, glass, tatto, moustache, pipe
+Eddie	glass
+Kelly	hair galss, eyepatch, earring, tatto, moustache
+Kennedy hair tatto eyepatch earring pipe
+Gabriel glass hair tatto earring pipe
+River   moustache 
+Frankie hat hair eyepatch pipe tie earring
+Val     hair eyepatch glass moustache pipe
+Dylan   eyepatch tatto earring pipe
+Caelan  earring moustache
+Dale	hair tatto moustache pipe earring glass eyepatch
+'''
