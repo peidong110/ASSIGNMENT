9294c031b9c9d4f7f8544456a0eb911a10ab06fd
Payton Dong Pei <silence1198@outlook.com>
2017-10-10 21:13:42 -0400

    Update comp1405_a4.py
    it perfect run this time

--- comp1405_a4.py
+++ comp1405_a4.py
@@ -1,12 +1,23 @@
+# ============================================================
+#
+# Student Name (as it appears on cuLearn): Payton Pei
+# Student ID (9 digits in angle brackets): <101065299>
+# Course Code (for this current semester): COMP1405B
+#
+# ============================================================
+
 # we are only allowed to use make_a_guess function and ask_question functio
 from comp1405_f17_assistant_a4 import *
 #question bank is showing below
-
-#decide whether it has mustache
+#the key here is that 2^4 is 16, so we use question to divide them like this 16/2=8 8/2=4 4/2=2 this we can get all the people to be selected.
+#divide them into two groups one is with mustache and another one does not have mustache
 questionMustache = ask_question("Does it have mustache?")
 if questionMustache.lower() == "yes":
+	#we ask if them have earring
     questionEarring = ask_question("Does it have earring?")
+
     if questionEarring.lower() == "yes":
+            #does it have pipe, if not divide them out
         questionPipe = ask_question("Does it have pipe?")
         if questionPipe.lower() == "yes":
             questionHat = ask_question("Does it have hat?")
@@ -34,13 +45,14 @@ if questionMustache.lower() == "yes":
                 make_a_guess("Val")
             else:
                 make_a_guess("River")
+#when they dont have mustache, divide them into two different groups
 else:
     questionGlassThirdUse = ask_question("Does it have glass?")
     if questionGlassThirdUse.lower() == "yes":
         questionEyepatch = ask_question("Does it have eyepaych?")
         if questionEyepatch.lower() == "yes":
             questionHatThirdUse = ask_question("Does it have hat?")
-            if questionHatThirdUse.lower() == "yer":
+            if questionHatThirdUse.lower() == "yes":
                 make_a_guess("Daryl")
             else:
                 make_a_guess("Mason")
@@ -64,7 +76,6 @@ else:
                 make_a_guess("Dylan")
             else:
                 make_a_guess("Tyler")
-
 
 
 
